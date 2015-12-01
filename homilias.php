25c059e71fad9102b0936e5ed8725f03dbe2c5f2
pauloparada <paulodavidparada@gmail.com>
2015-12-01 14:52:57 -0500

    changes

---
+++ homilias.php
@@ -0,0 +1,76 @@
+<?php
+echo'<div class="content">
+      <div class="container no-bottom">
+          <div class="heading">
+              <div class="heading-left">
+                  <!--<em>lates news and our</em>-->
+                  <h3>HOMILIAS DEL PADRE RAMÓN</h3>
+              </div>
+              <div class="heading-right">
+                  <span class="icon icon-blog"><img style="height:20px; width:20px; left:16%; top:16%;" src="images/calendar.png"></span>
+              </div>
+          </div>
+      </div>
+      
+      <div class="decoration"></div>
+      
+      <div class="container">
+          <div class="blog-post">
+              <div class="post-image">
+                  <img class="blog-post-image" src="images/blog/001.jpg" alt="img">  
+              </div>
+              <div class="post-content">
+                  <h3>HOMILÍA 11 DE MAYO DE 2014</h3>         
+                
+              </div>
+              <div class="post-details">
+                  <a class="posted-by" href="#">Admin</a>
+                  <a class="posted-at" href="#">Hace 2 Dias</a>
+                  <a class="posted-tag" href="#">Homilías, Mayo</a>
+                  <a class="posted-more" href="blog-post.html">Leer Más</a>
+                  <div class="clear"></div>
+              </div>
+          </div>
+      </div>
+
+  
+      <div class="decoration"></div>
+
+  
+      <div class="container">
+          <div class="blog-post">
+              <h3>HOMILÍA CUARTO DOMINGO DE PASCUA</h3> 
+              <img class="blog-post-image" src="images/blog/1.jpg" alt="img">       
+             
+              <div class="post-details">
+                  <a class="posted-by" href="#">Admin</a>
+                  <a class="posted-at" href="#">Hace 9 Dias</a>
+                  <a class="posted-tag" href="#">Homilías, Mayo</a>
+                  <a class="posted-more" href="blog-post.html">Leer Más</a>
+                  <div class="clear"></div>
+              </div>
+          </div>
+      </div> 
+      
+      <div class="decoration"></div>   
+       
+  
+      <div class="container no-bottom">
+          <div class="recent-post">
+              <p>
+                  <img src="images/blog/001.jpg" width="90" alt="img">
+                  <strong>HOMILÍA TERCER DOMINGO DE PASCUA</strong><br/>
+                  <br/> 
+                  <a href="#">Leer Más</a>
+              </p>
+          </div>
+          <div class="recent-post">
+              <p>
+                  <img src="images/blog/001.jpg" width="90" alt="img">
+                  <strong>HOMILÍA SEGUNDO DOMINGO DE PASCUA</strong><br/>
+                 <br/> 
+                  <a href="#">Leer Más</a>
+              </p>
+          </div>
+      </div>';
+?>
